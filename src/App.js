64d37a10ee345e0a1cacd47c0ec0d322ea77652c
AlannJoy <alldaubeterre@gmail.com>
2020-01-20 11:20:48 +0100

    Inizialiting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {" "}
+          I feel completely scared about learning React, but also excited!!
+        </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +19,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Crazy!!!
         </a>
       </header>
     </div>
